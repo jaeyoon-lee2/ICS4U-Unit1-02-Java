7644edb2cb9ebabfaddd038ce40bc00c7d3f1dae
Jay Lee <jaeyoon.lee@mths.ca>
2021-04-22 20:59:24 +0000

    update code

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-// package ca.mths.unit1.unit02.java.main;
-/**
+/*
 * This program gets the length of logs from the user's input,
 * calculates and display how much logs the truck can carry to the output.
 *
@@ -7,6 +6,7 @@
 * @version 1.0
 * @since   2020-04-22
 */
+// package ca.mths.unit1.unit02.java.main;
 
 import java.util.Scanner;
 
